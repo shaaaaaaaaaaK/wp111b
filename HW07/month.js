f0c19087d6da271ec77c937289d13ea8bdd77326
Shaaaaaaaaark <bella921127@gmail.com>
2023-05-07 09:41:24 +0800

    re

--- HW07/month.js
+++ HW07/month.js
@@ -2,18 +2,18 @@ function translate(english){
     return month[english];
 }
 var month={
-    "January":"一月",
-    "February":"二月", 
-    "March": "三月",
-    "April" :"四月",
-    "May" :"五月",
-    "June" :"六月",
-    "July" :"七月",
-    "August":"八月",
-    "September":"九月",
-    "October":"十月",
-    "November":"十一月" ,
-    "December":"十二月",
+    "January":"1",
+    "February":"2", 
+    "March": "3",
+    "April" :"4",
+    "May" :"5",
+    "June" :"6",
+    "July" :"7",
+    "August":"8",
+    "September":"9",
+    "October":"10",
+    "November":"11" ,
+    "December":"12",
 }
 
 console.log("January",translate("January"));
